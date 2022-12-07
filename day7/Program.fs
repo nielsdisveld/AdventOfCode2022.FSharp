ee1ef7b03d188a5e4056d177c031af0c66aa1822
nielsdisveld <Niels.Disveld@finaps.nl>
2022-12-07 22:39:56 +0100

    styling

--- day7/Program.fs
+++ day7/Program.fs
@@ -30,16 +30,19 @@ let toAbsolutePaths = // map each dir to its absolute path
     >> List.fold (fun (lst, absPath) dir -> (absPath+dir::lst, absPath+dir)) ([], "")
     >> fst
 // Solving
-let handleCommand (state: State) = function
+let handleCmd (state: State) = function
     | CdRoot -> {state with dirs = ["/"]}
     | CdOut -> { sizes = state.sizes; dirs = state.dirs |> List.tail}
     | Cd dirName -> {state with dirs = dirName::state.dirs}
     | Ls -> state
-let handleFile (state: State) (size: int64) =
-    { state with sizes = state.dirs |> toAbsolutePaths |> List.fold (add size) state.sizes }
+let updateSizes (state: State) (size: int64) =
+    { state with sizes =
+                    state.dirs
+                    |> toAbsolutePaths
+                    |> List.fold (add size) state.sizes }
 let folder (state: State) = function
-    | Command cmd -> cmd |> handleCommand state
-    | File size -> handleFile state size
+    | Command cmd -> cmd |> handleCmd state
+    | File size -> size |> updateSizes state
     | _ -> state
 let solve =
     Seq.fold folder { sizes = Map.empty; dirs = ["/"]}
