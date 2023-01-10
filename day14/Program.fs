da25f9b55f41566d9eae5c6569bbc874d0254cd2
nielsdisveld <Niels.Disveld@finaps.nl>
2023-01-10 16:05:24 +0100

    Much faster by using Map<int,Set<int>> for caves lookup

--- day14/Program.fs
+++ day14/Program.fs
@@ -1,8 +1,19 @@
 ﻿open Utils
 let file = "./input.txt"
+// Types
+type Caves = Map<int,Set<int>> // keys are x coordinates, a value are its all possible y values for an x
 // Helpers
 let generateArray n1 n2 = if n1 <= n2 then [|n1..n2|] else [|n2..n1|]
-let add c p = c |> Set.add p
+let addCave (m: Caves) (x,y) =
+    m.TryFind x
+    |> Option.defaultValue Set.empty
+    |> Set.add y
+    |> fun s -> m.Add (x,s)
+let contains (x,y) (m: Caves) =
+    match m.TryFind x with
+    | Some s -> Set.contains y s
+    | None -> false
+let count (caves: Caves) = caves.Values |> Seq.sumBy Set.count 
 // Parsing
 let parseCoordinate (str: string)  =
     match str.Split ',' with
@@ -16,41 +27,40 @@ let parseLine (str: string) =
     |> Array.map parseCoordinate
     |> Array.windowed 2
     |> Array.collect (fun coordinates -> drawPath coordinates[0] coordinates[1])
-let parseInput =
-    FileReading.readLines >>
+let transformInput =
     Seq.map parseLine >>
     Seq.collect id >>
-    Set.ofSeq
+    Seq.groupBy fst >>
+    Seq.map (fun (k,l) -> k, Seq.map snd l |> set) >>
+    Map.ofSeq
 // Solving
 let source = (500,0)
-let dropSand hasFloor floor caves =
+let dropSandUnit hasFloor floor (caves: Caves) =
     let rec loop (x0,y0) =
         [|x0,y0+1;x0-1,y0+1;x0+1,y0+1|]
-        |> Seq.tryFind (fun cave -> not (Set.contains cave caves))
+        |> Seq.tryFind (fun (x,y) -> not (contains (x,y) caves))
         |> function
-            | None when (x0,y0) = source -> add caves source, false
-            | None -> add caves (x0,y0), true
+            | None when (x0,y0) = source -> addCave caves source, false
+            | None -> addCave caves (x0,y0), true
             | Some (x,y) ->
                 if not hasFloor && y = (floor + 1) then caves, false
-                elif y = (floor + 1) then add caves (x,y), true
+                elif y = (floor + 1) then addCave caves (x,y), true
                 else loop (x,y)
     loop source
-let dropStream hasFloor rocks = 
-    let floor = rocks |> Seq.maxBy snd |> snd
+let dropSandStream hasFloor (rocks: Caves) = 
+    let floor = rocks.Values |> Seq.collect id |> Seq.max
     let rec loop sand =
-        match dropSand hasFloor floor sand with
-        | sand', true -> loop sand'
-        | sand', false -> sand'
-
-    Seq.length (loop rocks) - (Seq.length rocks)
-let solve1 = dropStream false
-let solve2 input = dropStream true input
+        match dropSandUnit hasFloor floor sand with
+        | caves', true -> loop caves'
+        | caves', false -> caves'
+    count (loop rocks) - (count rocks)
+let solve1 input = dropSandStream false input
+let solve2 input = dropSandStream true input
 // Solutions
-let analyze = Seq.length
-let run f = parseInput >> f 
+let run f = FileReading.readLines >> transformInput >> f 
 let solution1 = run solve1 file
 printfn $"Solution1: %A{solution1}"
 let solution2 = run solve2 file
 printfn $"Solution2: %A{solution2}"
 // Solution1: 795
-// Solution2: 30214
+// Solution2: 30214, speed: ~1666ms
