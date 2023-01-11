f9054dc2b570a7b7ccac64f3b33a57a2f7f86609
nielsdisveld <Niels.Disveld@finaps.nl>
2023-01-11 23:28:25 +0100

    Add parallel solving

--- day15/Program.fs
+++ day15/Program.fs
@@ -1,47 +1,56 @@
 ﻿open Utils
 let file = "./input.txt"
 // Helpers
-let contains x (i1,i2) = x>=i1&&x<=i2
-let rec findMissing intervals =
+let contains x (i1,i2) = (x >= i1) && (x <= i2)
+let rec findNotCovered intervals =
     let rec loop x  =
         match List.tryFind (contains x) intervals with
         | Some (_,i2) -> loop (i2+1)
         | None -> x
     loop 0
+let manhattanDist (x1,y1) (x2,y2) = abs (x2-x1) + abs (y2-y1)
 // Parsing
-let parseInt (c: char) (str: string) = str.Split c |> Array.head |> int
+let intPrecedingChar (c: char) (str: string) = str.Split c |> Array.head |> int
 let parseLine (str: string) =
     match str.Split '=' with
     | [|_;x1;y1;x2;y2|] ->
-        (parseInt ',' x1, parseInt ':' y1), (parseInt ',' x2, int y2)
-    | _ -> failwith $"Incorrect line: %i{(str.Split '=') |> Array.length}"
+        (intPrecedingChar ',' x1, intPrecedingChar ':' y1), (intPrecedingChar ',' x2, int y2)
+    | _ -> failwith $"Incorrect line: %A{str}"
+let transformInput input =
+    input
+    |> Seq.map parseLine
+    |> (Seq.map (fun (s,b) -> s, manhattanDist s b))
 // Solving
-let manhattanDist (x1,y1) (x2,y2) = abs (x2-x1) + abs (y2-y1)
-let coveredAt y0 ((sx,sy),d) =
+let getCover y0 ((sx,sy),d) =
     let dx = d - (abs (sy - y0))
     if dx < 0 then []
     else List.singleton (max 0 (sx-dx),min 4000000 (sx + dx))
-let solve0 y0 inp =
+let getCovers y0 inp =
     inp
-    |> Seq.map (coveredAt y0)
+    |> Seq.map (getCover y0)
     |> Seq.reduce List.append
-let solve inp =
-    let distances =
-        inp
-        |> Seq.map (fun (s,b) -> s, manhattanDist s b)
-    [|0..4000000|]
-    |> Array.tryPick (fun i ->
-        distances 
-        |> solve0 i
-        |> findMissing
-        |> function
-            | firstMiss when firstMiss <= 4000000 -> Some (firstMiss, i)
-            | _ -> None)
+let solve_y inp y =
+    inp 
+    |> getCovers y
+    |> findNotCovered
+    |> function
+        | n when n <= 4000000 -> Some (n, y) // return x,y coordinate of point not covered
+        | _ -> None
+let solve y_min y_max inp =
+    [|y_min..y_max|]
+    |> Array.tryPick (solve_y inp)
+let solveParallel inp =
+    [|0..3|]
+    |> Array.map (fun i -> async {return solve (i*1000000) ((i+1)*1000000) inp})
+    |> Async.Choice
+    |> Async.StartAsTask
+    |> fun task -> task.Result
+// Solutions
 let analyze = function
     | Some (x,y) -> 4000000L*(int64 x) + (int64 y)
     | None -> failwith "Point not found"
-let run = file |> FileReading.readLines |> Seq.map parseLine |> solve |> analyze
-let solution1 = run
-printfn $"Solution1: %A{solution1}"
+let run = file |> FileReading.readLines |> transformInput |> solveParallel |> analyze
+let solution2 = run
+printfn $"Solution1: %A{solution2}"
 // Solution1: 5127797
-// Solution2: 12518502636475L
+// Solution2: 12518502636475L speed: ~58.6s
