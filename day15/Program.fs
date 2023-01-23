efef00b29c7e3f8b86ce6eb909dd2af18d017d75
nielsdisveld <Niels.Disveld@finaps.nl>
2023-01-23 11:09:31 +0100

    styling

--- day15/Program.fs
+++ day15/Program.fs
@@ -4,11 +4,6 @@ let file = "./input.txt"
 // Line segment is defined by starting point, slope (can only be 1 or -1 in this scope) and length
 type LineSegment = (int * int) * int * int
 // Helpers
-let cartesian xs ys = xs |> Seq.collect (fun x -> ys |> Seq.map (fun y -> x,y))
-let filterIsSomeFolder lst opt =
-    match opt with
-    | Some v -> v::lst
-    | None -> lst
 let manhattanDist (x1,y1) (x2,y2) = abs (x2-x1) + abs (y2-y1)
 // Parsing
 let parseLine (str: string) =
@@ -24,34 +19,35 @@ let intersectDescAsc (((x1,y1),_,d1): LineSegment)  (((x2,y2),_,d2): LineSegment
     match x1+y1+x2-y2 with // Basically follows from solving this equality: -(x-x1)+y1=(x-x2)+y2
     | x' when
         (x' % 2) = 0 && // If x' is odd then there is no integer solution
-        x'/2>=x1 && x'/2<=(x1+d1) && x'/2>=x2 && x'/2<=x2+d2 -> Some (x'/2, y1-(x'/2-x1))
-    | _ -> None
+        x'/2>=x1 && x'/2<=(x1+d1) && x'/2>=x2 && x'/2<=x2+d2 -> [(x'/2, y1-(x'/2-x1))]
+    | _ -> []
 let rec intersection ((p1,slope1,d1): LineSegment, (p2,slope2,d2): LineSegment) =
     match slope1,slope2 with
     | -1,1 -> intersectDescAsc (p1,slope1,d1) (p2,slope2,d2)
     | 1,-1 -> intersectDescAsc (p2,slope2,d2) (p1,slope1,d1)
-    | _ -> None
+    | _ -> []
 let toBoundaries ((x,y), d') : LineSegment[] = // Get line segments that define the boundary of what a sensor is covering
     let d = d'+1
     [|(x-d,y),1,d; (x,y+d),-1,d; (x-d,y),-1,d; (x,y-d),1,d;|]
 // Solving
-let analyze = function
-    | Some (x,y) -> 4000000L*(int64 x) + (int64 y)
-    | None -> failwith "Point not found"
+let analyze (x,y) = 4000000L*(int64 x) + (int64 y)
 let allBoundaryIntersections (sensors: seq<(int*int)*int>) =
     sensors
     |> Seq.map toBoundaries // Map every sensor to its boundary
-    |> fun xs -> cartesian xs xs // Map to every combination of 2 boundaries
-    |> Seq.map (fun (x,y) -> cartesian x y) // Map to every combination of 2 line segments
-    |> Seq.collect (Seq.map intersection) // Collect every possible intersection point of 2 boundaries
-    |> Seq.fold filterIsSomeFolder []
+    |> fun s -> Seq.allPairs s s // Map to every combination of 2 boundaries
+    |> Seq.map (fun (x,y) -> Seq.allPairs x y) // Map to every combination of 2 line segments
+    |> Seq.collect (Seq.collect intersection) // Collect every possible intersection point of 2 boundaries
+let findUncovered (sensors, intersections) =
+    intersections
+    |> Seq.find (fun (x,y) ->
+    x>=0&&x<=4000000&&y>=0&&y<=4000000 &&
+    sensors |> Seq.forall (fun (s,d) -> manhattanDist (x,y) s > d)) // Uncovered point is outside every sensor range
 let solution =
-    let sensors = file |> FileReading.readLines |> transformInput
-    sensors
-    |> allBoundaryIntersections
-    |> List.tryFind (fun (x,y) ->
-        x>=0&&x<=4000000&&y>=0&&y<=4000000 &&
-        sensors|> Seq.forall (fun (s,d) -> manhattanDist (x,y) s > d)) // Find (x,y) that is outside every sensor cover
+    file
+    |> FileReading.readLines
+    |> transformInput
+    |> fun sensors -> sensors, allBoundaryIntersections sensors
+    |> findUncovered
     |> analyze
 printfn $"%A{solution}"
 // solution: 12518502636475
