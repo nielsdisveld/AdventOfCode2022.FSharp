97c3b697ff5369eee573caf33a218f7d1215f6fc
nielsdisveld <Niels.Disveld@finaps.nl>
2023-01-31 23:40:19 +0100

    dat 16 - part 2

--- day16/Program.fs
+++ day16/Program.fs
@@ -23,21 +23,21 @@ let edgesAndFlowRatesFolder (edges, flowRates) ((valve, flowRate), connectedValv
         if flowRate = 0 then set // No point in opening a valve with zero flow rate. i.e. adding an edge from a valve to itself
         else Set.add (valve, valve) set)
     , flowRates |> Map.add valve flowRate
-let edgesPerMin (v1,v2) = // We need to interpret each edge as movement + movement in time
-    [|1..29|]
+let edgesPerMin j (v1,v2) = // We need to interpret each edge as movement + movement in time
+    [|1..j-1|]
     |> Array.map (fun i -> (v1,i),(v2,i+1))
     |> Set.ofArray
-let toAllEdgesPerMin = Set.map edgesPerMin >> Set.unionMany
-let toNetReleasePerMin valve flowRate = [|1..30|] |> Array.map (fun i -> (valve,i),(31-i) * flowRate) // It is easier to translate flow rate to net release (which depends on the minute it was opened)
-let toAllNetReleases (flowRates: Map<string,int>)=
+let toAllEdgesPerMin i = Set.map (edgesPerMin i) >> Set.unionMany
+let toNetReleasePerMin j valve flowRate = [|1..j|] |> Array.map (fun i -> (valve,i),((j+1)-i) * flowRate) // It is easier to translate flow rate to net release (which depends on the minute it was opened)
+let toAllNetReleases i (flowRates: Map<string,int>)=
     flowRates
-    |> Seq.collect (fun kv -> toNetReleasePerMin kv.Key kv.Value)
+    |> Seq.collect (fun kv -> toNetReleasePerMin i kv.Key kv.Value)
     |> Map.ofSeq
-let transformInput =
+let transformInput i =
     FileReading.readLines
     >> Seq.map parseLine
     >> Seq.fold edgesAndFlowRatesFolder (Set.empty, Map.empty)
-    >> fun (edges, vertices) -> toAllEdgesPerMin edges, toAllNetReleases vertices
+    >> fun (edges, vertices) -> toAllEdgesPerMin i edges, toAllNetReleases i vertices
 // Solving
 // We cannot just keep track of total release per vertex, we need to keep track of different sets of opened valves as well
 type TotalReleases = Map<string*int, Map<Set<string>,int>> 
@@ -77,14 +77,35 @@ let solve (edges, flowRates: Map<string*int,int>)=
     
     let initial = Map.empty.Add (("AA", 1), Map.empty.Add (Set.empty, 0))
     loop initial edges
-// Solution
-let analyze (totalReleases: TotalReleases) =
+// Analyzing for part 1
+let analyzePart1 (totalReleases: TotalReleases) =
     totalReleases
     |> Seq.collect (fun kv ->
         kv.Value
         |> Seq.map (fun kv'-> kv'.Value))
     |> Seq.max
-let run = transformInput >> solve >> analyze
-let solution = run file
-printfn $"%A{solution}"
+// Analyzing for part 2
+let find2HighestNonOverlapping (releases: seq<Set<string>*int>) =
+    releases
+    |> Seq.mapi (fun i x -> i,x)
+    |> Seq.pick (fun (i, (opened, v1)) ->    
+            releases
+            |> Seq.take (i+1)
+            |> Seq.tryFind (fun (opened', _) -> Set.intersect opened opened' |> Set.isEmpty)
+            |> Option.map (fun (_,v2) -> v1 + v2))
+let analyzePart2 (totalReleases: TotalReleases) =
+    totalReleases
+    |> Seq.collect (fun kv ->
+        kv.Value
+        |> Seq.filter (fun kv' -> kv'.Key <> Set.empty && 26 = (kv.Key |> snd ))
+        |> Seq.map (fun kv' -> kv'.Key, kv'.Value))
+    |> Seq.sortByDescending snd
+    |> find2HighestNonOverlapping
+// Run
+let run i f = transformInput i >> solve >> f
+let solution1 = run 30 analyzePart1 file
+printfn $"%i{solution1}"
+let solution2 = run 26 analyzePart2 file
+printfn $"%i{solution2}"
 // Solution1: 1741
+// Solution2: 2316
